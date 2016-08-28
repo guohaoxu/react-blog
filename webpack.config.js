8100d455990a237dde68eb7629b56042e6f09c5d
guohao <guohao0817@gmail.com>
2016-08-29 00:52:37 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: [
+    './index.js'
+  ],
+  output: {
+    path: __dirname + '/dist/js',
+    publicPath: '/static/js',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
