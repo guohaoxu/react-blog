6f3aa95f93bc00b18924fab49d07d4184011c33c
guohao <guohao0817@gmail.com>
2016-09-03 00:39:22 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,12 @@ var gulp = require('gulp'),
   imagemin = require('gulp-imagemin'),
   rename = require('gulp-rename'),
   revCollector = require('gulp-rev-collector'),
+  revReplace = require('gulp-rev-replace')
   minifyHTML = require('gulp-minify-html'),
 
   mocha = require('gulp-mocha'),
-  eslint = require('gulp-eslint')
+  eslint = require('gulp-eslint'),
+  hash = require('gulp-hash')
 
 gulp.task('lint', () => {
   gulp.src(['**/*.js', '!node_modules/**', '!build/**', '!dist/bootstrap/**'])
@@ -18,11 +20,39 @@ gulp.task('lint', () => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 })
-gulp.task('build', ['lint'], () => {
+gulp.task('build', () => {
+  gulp.src('./dist/images/*')
+    // .pipe(imagemin())
+    .pipe(hash())
+    .pipe(gulp.dest('./build/images/'))
+    .pipe(hash.manifest('hash.json'))
+    .pipe(gulp.dest('./build/'))
 
-  gulp.src('./dist/uploads/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/uploads/'))
+    gulp.src(['./dist/bootstrap/css/bootstrap.min.css', './dist/css/*.css'])
+      .pipe(concat('all.css'))
+      .pipe(cleanCSS())
+      .pipe(hash())
+      .pipe(gulp.dest('./build/stylesheets/'))
+      .pipe(hash.manifest('hash.json'))
+      .pipe(gulp.dest('./build/'))
+
+    gulp.src(['./dist/js/bundle.js', './dist/js/jquery-1.12.1.min.js', './dist/bootstrap/js/bootstrap.min.js'])
+      .pipe(concat('all.js', {newLine: ';'}))
+      .pipe(uglify())
+      .pipe(hash())
+      .pipe(gulp.dest('./build/javascripts/'))
+      .pipe(hash.manifest('hash.json'))
+      .pipe(gulp.dest('./build/'))
+
+    gulp.src('./dist/favicon.ico')
+      .pipe(gulp.dest('./build/'))
+
+    gulp.src('./dist/bootstrap/fonts/*')
+      .pipe(gulp.dest('./build/fonts/'))
+
+    gulp.src('./dist/uploads/*')
+      // .pipe(imagemin())
+      .pipe(gulp.dest('./build/uploads/'))
 
   gulp.src('./index_dev.ejs')
     .pipe(rename('./index.ejs'))
@@ -32,44 +62,29 @@ gulp.task('build', ['lint'], () => {
     }))
     .pipe(gulp.dest('./'))
 
-  gulp.src('./dist/favicon.ico')
-    .pipe(gulp.dest('./build/'))
-
-  gulp.src(['./dist/bootstrap/css/bootstrap.min.css', './dist/css/main.css'])
-    .pipe(concat('all.css'))
-    .pipe(cleanCSS())
-    .pipe(rev())
-    .pipe(gulp.dest('./build/stylesheets/'))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('./build/stylesheets/'))
-
-  gulp.src(['./dist/js/bundle.js', './dist/js/jquery-1.12.1.min.js', './dist/bootstrap/js/bootstrap.min.js'])
-    .pipe(concat('all.js', {newLine: ';'}))
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./build/javascripts/'))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('./build/javascripts/'))
-
-  gulp.src('./dist/bootstrap/fonts/*')
-    .pipe(gulp.dest('./build/fonts/'))
-
 })
 
+
 gulp.task('default', ['build'], () => {
-  gulp.src(['./build/**/*.json', './index.ejs'])
+  gulp.src(['./build/hash.json', './build/stylesheets/*.css'])
+    .pipe(revCollector())
+    .pipe(gulp.dest('./'))
+
+  gulp.src(['./build/hash.json', './index.ejs'])
     .pipe(revCollector({
-      replaceReved: true,
-      dirReplacements: {
-        '/static/stylesheets/': (manifest_value) => {
-          return '//localhost:3000' + '/static/stylesheets/' + manifest_value
-        },
-        '/static/javascripts/': (manifest_value) => {
-          return '//localhost:3000' + '/static/javascripts/' + manifest_value
-        }
-      }
+      // dirReplacements: {
+      //   '/static/stylesheets/': (manifest_value) => {
+      //     return '//localhost:3000' + '/static/stylesheets/' + manifest_value
+      //   },
+      //   '/static/javascripts/': (manifest_value) => {
+      //     return '//localhost:3000' + '/static/javascripts/' + manifest_value
+      //   }
+      // }
     }))
     .pipe(gulp.dest('./'))
+
+
+    // gulp.watch(['**/*.js', '!node_modules/**', '!build/**', '!dist/bootstrap/**'], ['lint'])
 })
 
 gulp.task('test', () => {
